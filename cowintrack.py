6379ffd57e01aea23e879f55bb5db648b4867e0e
Jothish <jothishwp8@outlook.com>
2021-07-12 18:56:44 +0530

    working

--- cowintrack.py
+++ cowintrack.py
@@ -2,51 +2,120 @@ import requests
 import json
 from datetime import date
 from flask import Flask, jsonify, request
+import time
+import csv
+from fake_useragent import UserAgent
+
+ua = UserAgent()
 
 today = date.today()
 app=Flask(__name__)
 
-def listOut():
+# class Config(object):
+#     SCHEDULER_API_ENABLED = True
 
+# app.config.from_object(Config())
+
+# scheduler = APScheduler()
+# # if you don't wanna use a config, you can set options here:
+# # scheduler.api_enabled = True
+# scheduler.init_app(app)
+# scheduler.start()
+
+def listOut():
 	header= {
 		"Accept": "application/json, text/plain, */*",
 		"Accept-Encoding": "gzip, deflate, br",
-		"Accept-Language": "en-GB",
-		"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJlNjI0OGI1YS1mMDI5LTQ2OWEtOTI2OC1kMDA4NzU3YzQyYTkiLCJ1c2VyX2lkIjoiZTYyNDhiNWEtZjAyOS00NjlhLTkyNjgtZDAwODc1N2M0MmE5IiwidXNlcl90eXBlIjoiQkVORUZJQ0lBUlkiLCJtb2JpbGVfbnVtYmVyIjo5ODA5ODY0Mjc1LCJiZW5lZmljaWFyeV9yZWZlcmVuY2VfaWQiOjc1OTExNDUzNTA1MDMwLCJzZWNyZXRfa2V5IjoiYjVjYWIxNjctNzk3Ny00ZGYxLTgwMjctYTYzYWExNDRmMDRlIiwic291cmNlIjoiY293aW4iLCJ1YSI6Ik1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NDsgcnY6ODguMCkgR2Vja28vMjAxMDAxMDEgRmlyZWZveC84OC4wIiwiZGF0ZV9tb2RpZmllZCI6IjIwMjEtMDUtMjhUMTE6Mzc6MTEuOTk2WiIsImlhdCI6MTYyMjIwMTgzMSwiZXhwIjoxNjIyMjAyNzMxfQ.JJ_TMYQ95taOFbJ3lRN3RPe_sTXqiNBUzIgoxyAJbOU",
+		"Accept-Language": "en-AU,en;q=0.9,en-IN;q=0.8",
+		"Authorization": str((open("auth.txt","r")).read().strip()),
 		"Connection": "keep-alive",
 		"Host": "cdn-api.co-vin.in",
 		"Origin": "https://selfregistration.cowin.gov.in",
 		"Referer": "https://selfregistration.cowin.gov.in/",
 		"TE": "Trailers",
-		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0"
+		"User-Agent": str(ua.chrome),
 	}
+	# krlDst=[{"district_id": 301,"district_name": "Alappuzha"},{"district_id": 307,"district_name": "Ernakulam"},{"district_id": 306,"district_name": "Idukki"},{"district_id": 297,"district_name": "Kannur"},{"district_id": 295,"district_name": "Kasaragod"},{"district_id": 298,"district_name": "Kollam"},{"district_id": 304,"district_name": "Kottayam"},{"district_id": 305,"district_name": "Kozhikode"},{"district_id": 302,"district_name": "Malappuram"},{"district_id": 308,"district_name": "Palakkad"},{"district_id": 300,"district_name": "Pathanamthitta"},{"district_id": 296,"district_name": "Thiruvananthapuram"},{"district_id": 303,"district_name": "Thrissur"},{"district_id": 299,"district_name": "Wayanad"}]
 	s =requests.session()
+	mainList=[]
+	# for d in krlDst:	
 	output=s.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=307&date="+today.strftime("%d-%m-%Y"),headers=header).text
+	#print(output)
 	output=json.loads(output)
 	centers=output["centers"]
-
-	mainList=[]
+	# print(centers)
 	for i in centers:
-		tempList=[i["center_id"],i["name"],i["pincode"],i["fee_type"]]
+		tempList=[i["center_id"],i["name"],i["pincode"],i["fee_type"],i["district_name"]]
 		# print(len(i["sessions"]))
+		dataCsv = csv.reader(open('center.csv', "r"), delimiter=",")
+		fileStatus=True
+		for cent in dataCsv:
+			if str(i["center_id"]) in cent:
+				fileStatus=False
+		if fileStatus:
+			file=open("center.txt","a+")
+			file.write(str(i["center_id"])+","+i["name"]+","+i["address"]+","+i["state_name"]+","+i["district_name"]+","+i["block_name"]+","+str(i["pincode"])+","+str(i["lat"])+","+str(i["long"])+"\n")
+			with open('center.csv', 'a', newline='') as file:
+				writer = csv.writer(file)
+				writer.writerow([str(i["center_id"]),i["name"],i["address"],i["state_name"],i["district_name"],i["block_name"],str(i["pincode"]),str(i["lat"]),str(i["long"])])				
 		aviDoses=0
+		aviDosesPople=0
+		aviDosesEmp=0
 		for b in i["sessions"]:
 			aviDoses=aviDoses+b["available_capacity"]
+			aviDosesPople=aviDosesPople+b["available_capacity_dose1"]
+			aviDosesEmp=aviDosesEmp+b["available_capacity_dose2"]
 			tempList.append([b["date"],b["available_capacity"],b["min_age_limit"],b["vaccine"],b["available_capacity_dose1"],b["available_capacity_dose2"]])
+		tempList.append(aviDosesPople)
+		tempList.append(aviDosesEmp)
 		tempList.append(aviDoses)
 		mainList.append(tempList)
+		# time.sleep(1)
 	def myFunc(e):
 		return e[-1]
 	mainList.sort(key=myFunc,reverse=True)
-	return mainList
-	print(mainList)
+	finaList=[]
+	for items in mainList:
+		if items[-1]!=0:
+			finaList.append(items)
+	#print(finaList)
+	return finaList
 
-@app.route('/', methods = ['GET', 'POST'])
+
+# @scheduler.task('interval', id='do_job_1', seconds=65, misfire_grace_time=5)
+# def job1():
+#     print ("updated")
+#     listOut()
+
+@app.route("/",methods=['POST','GET'])                #first line / represents the route dir 
 def home():
 	if(request.method == 'GET'):
 		data = listOut()
-		return (jsonify({'data': data}))
+		def myFunc(e):
+			return e[-4]
+		data.sort(key=myFunc)
 
+		don="<html><head><meta http-equiv='refresh' content='30'></head><table><tr>"
+		if len(data)>0:
+			for i in data:
+				don=don+"<td> District: <b>"+str(i[4])+"</b></td><td> Center name: <b>"+str(i[1])+"</b></td><td> pincode: <b>"+str(i[2])+"</b> </td><td> Free/Paid: <b>"+str(i[3])+"</b> </td><td> people portal: <b>"+str(i[-3])+"</b> </td><td> for employees portal : <b>"+str(i[-2])+"</b> </td><td> total : <b>"+str(i[-1])+"</b> </td> </tr>"
+			return don+"</table></html>"
+		else:
+			return "<h1>nothing avilable</h1>"
+	else:
+		return("request correctly")
 
 if __name__ == "__main__":#check wether the ___name__ == __main__ conforming only devoloper can debug the code... if this command is not here anyone can access and debug the code..
     app.run()
+
+
+
+
+	# "content-type": "application/json",
+	# 	"content-length": "141",
+	# 	"dnt": "1",
+	# 	"sec-ch-ua": "'Chromium';v='91', ' Not;A Brand';v='99'",
+	# 	"sec-ch-ua-mobile": "?0",
+	# 	"sec-fetch-dest": "empty",
+	# 	"sec-fetch-mode": "cors",
+	# 	"sec-fetch-site": "cross-site"
